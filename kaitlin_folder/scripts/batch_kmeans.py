ab065c31f756baad336a3bfc1659fe69db708aae
kpet <kpet@iu.edu>
2021-06-03 13:19:56 -0400

    working high throughput script

--- kaitlin_folder/scripts/batch_kmeans.py
+++ kaitlin_folder/scripts/batch_kmeans.py
@@ -4,21 +4,25 @@ from dipy.io.image import load_nifti
 import pandas as pd
 import sklearn.cluster
 import pathlib
-
+import sys
+import os
+from multiprocessing import Pool
 
 '''
 Path to directory containing Brats data (probably LGG or HGG)
 '''
 
 #path_to_lib = "../../lib/MICCAI_BraTS2020_TrainingData"
-path_to_lib = "/N/slate/kpet/LAIDEL-MRI-tumor-id/lib/BraTS_2018/HGG"
+path_to_lib = "/N/project/laidel_el_mcv/LAIDEL-MRI-tumor-id/lib/MICCAI_BraTS2020_TrainingData" 
+#first command line argument
+#path_to_lib = sys.argv[1]
 
 '''
 path to results
 '''
-path_to_results = "/N/slate/kpet/LAIDEL-MRI-tumor-id/kaitlin_folder/results"
-
-
+#second command line argument
+#path_to_results=sys.argv[2]
+path_to_results_folder ="/N/project/laidel_el_mcv/LAIDEL-MRI-tumor-id/results/kmeans-kaitlin/"
 '''
 Function for finding mean and varieance of each cluster
 '''
@@ -28,6 +32,39 @@ def calc_mean_var(flair_2D, flair_kmeans, clusters):
         section=flair_2D[flair_kmeans==i]
         kmeans_arr[i]=np.array([np.mean(section), np.std(section)])
     return kmeans_arr
+
+
+'''
+Body of list comprehension - hopefully
+'''
+def loop_body(folder_path):
+
+  if folder_path.is_file() == False:
+    #extract ID of data file, used for naming
+    data_id = folder_path.parts[len(folder_path.parts)-1]
+    #create folder with data ID in results folder 
+    #path_to_results = path_to_results_folder+str(data_id)+'/'
+    #os.system('mkdir '+path_to_results )
+
+    for path in pathlib.Path(folder_path).iterdir():
+        #check if file is FLAIR
+        if path.is_file() and ("flair" in str(path)):
+            
+            #print(data_id)
+            data_flair, affine, im = load_nifti(path, return_img=True)
+            
+            #reshape
+            flair_2D = data_flair.reshape(flat_length, 1) #kmeans needs 2d (vertical) matrix where each row is a datapoint
+            flair_kmeans = sklearn.cluster.KMeans(n_clusters=clusters, random_state=0).fit_predict(flair_2D)
+            
+            
+            flair_kmeans_brain = flair_kmeans.reshape(240, 240, 155)
+           
+            #write data output
+            np.save(path_to_results_folder+data_id+".npy", flair_kmeans_brain, allow_pickle=True, fix_imports=True)
+            #mean_var_list.append(calc_mean_var(flair_2D, flair_kmeans, clusters))
+            #data_order_ref.append(data_id)
+
 
 '''
 total clusters in FLAIR image (background, WM , GM, tumor)
@@ -42,40 +79,27 @@ flat_length=240*240*155
 '''
 holds names of MRI data identifiers in the order they were accessed
 '''
-data_order_ref = []
+#data_order_ref = []
 
 '''
 holds cluster means and variances
 '''
-mean_var_list = []
+#mean_var_list = []
 
 
 '''
 Iterate through each MRI folder, extract FLAIR image, and convert to kmeans
 '''
-for folder_path in pathlib.Path(path_to_lib).iterdir():
-    print(str(folder_path))
-    data_id = folder_path.parts[len(folder_path.parts)-1]
-    for path in pathlib.Path(folder_path).iterdir():
-        #check if file is FLAIR
-        if path.is_file() and ("flair" in str(path)):
-            
-            print(data_id)
-            data_flair, affine, im = load_nifti(path, return_img=True)
-            
-            #reshape
-            flair_2D = data_flair.reshape(flat_length, 1) #kmeans needs 2d (vertical) matrix where each row is a datapoint
-            flair_kmeans = sklearn.cluster.KMeans(n_clusters=clusters, random_state=0).fit_predict(flair_2D)
-            
-            
-            flair_kmeans_brain = flair_kmeans.reshape(240, 240, 155)
-           
-            #write data output
-            np.save(path_to_results+data_id+".npy", flair_kmeans_brain, allow_pickle=True, fix_imports=True)
-            mean_var_list.append(calc_mean_var(flair_2D, flair_kmeans, clusters))
-            data_order_ref.append(data_id)
 
-            plt.show()
-dct = {"names": np.asarray(data_order_ref),"data": np.asarray(mean_var_list)}
-np.savez(path_to_results+"/aggregate.npz", **dct)
+#list comprehension
+#[ loop_body(folder_path) for folder_path in pathlib.Path(path_to_lib).iterdir()]
+ 
+#mutlithreading
+with Pool(processes=8) as pool:
+	pool.map(loop_body, pathlib.Path(path_to_lib).iterdir())           
+
+
+#move this to different script
+#dct = {"names": np.asarray(data_order_ref),"data": np.asarray(mean_var_list)}
+#np.savez(path_to_results_folder+"/aggregate.npz", **dct)
 
